313bc6c8183b6466f5d9b039c5dc90442ada5921
Tommy McGahee <admin@example.com>
2017-07-22 15:34:07 -0400

    Update git-to-puppet-environments.sh

--- scripts/git-to-puppet-environments.sh
+++ scripts/git-to-puppet-environments.sh
@@ -5,7 +5,7 @@ sudo git -C $BASEPATH fetch --all
 if [ -d "$BASEPATH/$CI_COMMIT_REF_NAME" ]; then 
     sudo  git -C $BASEPATH/$CI_COMMIT_REF_NAME reset --hard "origin/$CI_COMMIT_REF_NAME" 
 else 
-    sudo git clone $BASEPATH --branch "$CI_COMMIT_REF_NAME" 
+    sudo git clone git@gitlab.mcgahee.homelab:homelab/puppet.git $BASEPATH --branch "$CI_COMMIT_REF_NAME" 
 fi
 
 sudo librarian-puppet install --clean --verbose --path=$BASEPATH/$CI_COMMIT_REF_NAME/modules/
