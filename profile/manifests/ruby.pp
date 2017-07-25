fad2a06ad4fcc1ca41fe570d4625d52c1996fd24
Tommy McGahee <admin@example.com>
2017-07-25 02:42:38 -0400

    Update ruby.pp

--- profile/manifests/ruby.pp
+++ profile/manifests/ruby.pp
@@ -1,5 +1,7 @@
-class { '::ruby':
-  version         => '2.4.1', 
-  gems_version    => 'latest', 
-  rubygems_update => false,
+class profile::ruby {
+    class { '::ruby':
+      version         => '2.4.1', 
+      gems_version    => 'latest', 
+      rubygems_update => false,
+    }
 }
