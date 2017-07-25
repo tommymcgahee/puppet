59e00c4840f95c5cc63e1e46f50e80b2bc2cd4bc
Tommy McGahee <admin@example.com>
2017-07-25 02:41:10 -0400

    Update gitlab_community_edition_omnibus.pp

--- profile/manifests/gitlab_community_edition_omnibus.pp
+++ profile/manifests/gitlab_community_edition_omnibus.pp
@@ -1,20 +1,22 @@
-include postfix
-
-postfix::config { 'relay_domains':
-  ensure  => present,
-  value   => 'localhost gitlab.mcgahee.homelab',
-}
-
-class { 'gitlab':
-  external_url => 'https://gitlab.mcgahee.homelab',
-  nginx        => {
-     redirect_http_to_https => true,
-  },
-  gitlab_rails => {
-    'webhook_timeout' => 10,
-    'gitlab_default_theme' => 2,
-  },
-  logging      => {
-    'svlogd_size' => '200 * 1024 * 1024',
-  },
+class profile::gitlab_community_edition_omnibus {
+    include postfix
+    
+    postfix::config { 'relay_domains':
+      ensure  => present,
+      value   => 'localhost gitlab.mcgahee.homelab',
+    }
+    
+    class { 'gitlab':
+      external_url => 'https://gitlab.mcgahee.homelab',
+      nginx        => {
+         redirect_http_to_https => true,
+      },
+      gitlab_rails => {
+        'webhook_timeout' => 10,
+        'gitlab_default_theme' => 2,
+      },
+      logging      => {
+        'svlogd_size' => '200 * 1024 * 1024',
+      },
+    }
 }
