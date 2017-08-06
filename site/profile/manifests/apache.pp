1e283281c50a65c980525553fe5f6aa330f81742
Tommy McGahee <admin@example.com>
2017-08-06 15:44:27 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,13 +1,9 @@
 class profile::apache {
 
-    # Include Apache
     class { 'apache':
         default_vhost => false,
     }
   
-    # Create a hash from Hiera Data with the Vhosts
     $myApacheVhosts = hiera('apache::vhosts', {})
-
-    # With Create Resource Converts a hash into a set of resources
     create_resources('apache::vhost', $myApacheVhosts)
 }
