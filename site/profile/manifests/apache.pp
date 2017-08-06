5b231e5f87989972a562495cf7e557ee6ea9fcc3
Tommy McGahee <admin@example.com>
2017-08-06 17:04:22 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,17 +1,19 @@
 class profile::apache {
-
-    exec {'create_self_signed_sslcert':
-        command => "openssl req -newkey rsa:2048 -nodes -keyout ${::fqdn}.key  -x509 -days 365 -out ${::fqdn}.crt -subj '/CN=${::fqdn}'",
-        cwd     => "/etc/apache2/ssl/",
-        creates => [ "/etc/apache2/ssl/${::fqdn}.key", "/etc/apache2/ssl/${::fqdn}.crt", ],
-        path    => ["/usr/bin", "/usr/sbin"]
-    }
     
     class { 'apache':
         default_vhost => false,
     }
 
-  
     $myApacheVhosts = hiera('apache::vhosts', {})
-    create_resources('apache::vhost', $myApacheVhosts)
+
+    $createSelfSignedCerts = {
+        exec {'create_self_signed_sslcert':
+            command => "openssl req -newkey rsa:2048 -nodes -keyout ${apache::vhost::vhost_name}.key  -x509 -days 365 -out ${apache::vhost::vhost_name}.crt -subj '/CN=${apache::vhost::vhost_name}'",
+            cwd     => "/etc/ssl/",
+            creates => [ "/etc/ssl/${apache::vhost::vhost_name}.key", "/etc/ssl/${apache::vhost::vhost_name}.crt", ],
+            path    => ["/usr/bin", "/usr/sbin"]
+        }
+    }
+    
+    create_resources('apache::vhost', $myApacheVhosts, $createSelfSignedCerts)
 }
