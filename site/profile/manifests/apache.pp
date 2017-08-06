e693c7c6d05bcfa14503e72919e906212e3bb11e
Tommy McGahee <admin@example.com>
2017-08-06 16:04:34 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,8 +1,8 @@
 class profile::apache {
 
-    class { 'apache':
-        default_vhost => false,
-    }
+    #class { 'apache':
+    #    default_vhost => false,
+    #}
     
     exec {'create_self_signed_sslcert':
         command => "openssl req -newkey rsa:2048 -nodes -keyout ${::fqdn}.key  -x509 -days 365 -out ${::fqdn}.crt -subj '/CN=${::fqdn}'",
