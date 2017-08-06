62fe7f19d5118bb36897aff9acf15c602b609160
Tommy McGahee <admin@example.com>
2017-08-06 16:00:09 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,6 +3,13 @@ class profile::apache {
     class { 'apache':
         default_vhost => false,
     }
+    
+    exec {'create_self_signed_sslcert':
+        command => "openssl req -newkey rsa:2048 -nodes -keyout ${::fqdn}.key  -x509 -days 365 -out ${::fqdn}.crt -subj '/CN=${::fqdn}'",
+        cwd     => $certdir,
+        creates => [ "${certdir}/${::fqdn}.key", "${certdir}/${::fqdn}.crt", ],
+        path    => ["/usr/bin", "/usr/sbin"]
+    }
   
     $myApacheVhosts = hiera('apache::vhosts', {})
     create_resources('apache::vhost', $myApacheVhosts)
