1ec031486622d9219fe898ad6a0d163693ce7014
Tommy McGahee <admin@example.com>
2017-08-06 16:29:20 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,8 +2,8 @@ class profile::apache {
 
     exec {'create_self_signed_sslcert':
         command => "openssl req -newkey rsa:2048 -nodes -keyout ${::fqdn}.key  -x509 -days 365 -out ${::fqdn}.crt -subj '/CN=${::fqdn}'",
-        cwd     => "/etc/apach2/ssl/",
-        creates => [ "/etc/apach2/ssl/${::fqdn}.key", "/etc/apach2/ssl/${::fqdn}.crt", ],
+        cwd     => "/etc/apache2/ssl/",
+        creates => [ "/etc/apache2/ssl/${::fqdn}.key", "/etc/apache2/ssl/${::fqdn}.crt", ],
         path    => ["/usr/bin", "/usr/sbin"]
     }
     
